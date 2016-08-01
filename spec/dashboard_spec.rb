3ae0e0d8a90f02d14d98f92703dac0422a3d741a
Mayra Cabrera <mcabrera1087@gmail.com>
2016-07-31 19:51:03 -0500

    Use double, allow & with to clean up test logic

--- spec/dashboard_spec.rb
+++ spec/dashboard_spec.rb
@@ -5,22 +5,12 @@ describe Dashboard do
   describe "#posts" do
     it "returns posts visible to the current user" do
       user = create(:user)
-      other_user = create(:user)
-      create :post, user: other_user, published: true, title: "published_one"
-      create :post, user: other_user, published: true, title: "published_two"
-      create :post, user: other_user, published: false, title: "unpublished"
-      create :post, user: user, published: false, title: "visible_one"
-      create :post, user: user, published: false, title: "visible_two"
+      posts = double(posts)
+      allow(Post).to receive("visible_to").with(user).and_return(posts)
       dashboard = Dashboard.new(posts: Post.all, user: user)
 
-      result = dashboard.posts
 
-      expect(result.map(&:title)).to match_array(%w(
-        published_one
-        published_two
-        visible_one
-        visible_two
-      ))
+      expect(dashboard.posts).to eq(posts)
     end
   end
 end
